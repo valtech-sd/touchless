c8d3efec9d725e3e979c6773573c73ebbbd44e6b
Jason Alderman <jason.alderman@mjdinteractive.com>
2020-08-14 18:26:58 -0700

    Prototype for touchless gaze detection.

---
+++ docs/gaze/README.md
@@ -0,0 +1,113 @@
+# Gaze detection demo
+
+This is a prototype that uses the direction of your face to control a cursor on a screen. It builds on Google's Apache 2.0 licensed Facemesh demo found [here](https://github.com/tensorflow/tfjs-models/tree/master/facemesh/demo).
+
+To run it locally, we have to use a bundler that takes the node packages and compiles them into a static .js file. We're following Google's example here and using [Parcel](https://parceljs.org) because they've already configured it for us. 
+
+## Installation
+
+1. Run `npm i` from within this directory to install the node modules.
+2. Run `npm dev` to build everything into the `./dist` folder and run a local server at `localhost:1234`.
+3. Run `npm build` to build it for production.
+
+## How it works
+
+[Facemesh](https://github.com/tensorflow/tfjs-models/tree/master/facemesh) is a machine learning model that can look at an image or video and make a 3D mesh of a face (with 486 reference points). It runs entirely locally on the browser, using a TensorFlow model that has been created and optimized for TensorFlow.js by Google's team. 
+
+The facemesh model, along with handpose, is also a part of [Mediapipe](https://google.github.io/mediapipe/), an initiative from the TensorFlow team to allow easier combinations of different ML models for use on devices, desktops, and the web.
+
+## What it returns
+
+Each camera frame that is processed returns an array of `prediction` objects (one `prediction` for each estimated face the model thinks it sees in the image). A `prediction` object has the following properties:
+
+- **`faceInViewConfidence`** - (`0` to `1`) how confident the model is that this is a face, with a default threshold of 75%.
+- **`boundingBox`** - the 2D coordinates in the video image for `topLeft` and `bottomRight` of the bounding box. The tfjs facemesh model currently does not support rotation of the face or its bounding box, like the mediapipe gifs suggest.
+- **`mesh`** - the mesh of 3D points (array of `[x,y,z]`)
+- **`scaledMesh`** - the mesh of 3D points, normalized to the center of the bounds _(of all predicted faces?)_
+- **`annotations`** - arrays of 3D points in `scaledMesh` that might be useful to you!  
+Listed below as _`annotationName` (number of points)_
+     - `silhouette` (36)
+     - `lipsUpperOuter` (11)
+     - `lipsLowerOuter` (10)
+     - `lipsUpperInner` (11)
+     - `lipsLowerInner` (11)
+     - `rightEyeUpper0` (7)
+     - `rightEyeLower0` (9)
+     - `rightEyeUpper1` (7)
+     - `rightEyeLower1` (9)
+     - `rightEyeUpper2` (7)
+     - `rightEyeLower2` (9)
+     - `rightEyeLower3` (9)
+     - `rightEyebrowUpper` (8)
+     - `rightEyebrowLower` (6)
+     - `leftEyeUpper0` (7)
+     - `leftEyeLower0` (9)
+     - `leftEyeUpper1` (7)
+     - `leftEyeLower1` (9)
+     - `leftEyeUpper2` (7)
+     - `leftEyeLower2` (9)
+     - `leftEyeLower3` (9)
+     - `leftEyebrowUpper` (8)
+     - `leftEyebrowLower` (6)
+     - `midwayBetweenEyes` (1)
+     - `noseTip` (1)
+     - `noseBottom` (1)
+     - `noseRightCorner` (1)
+     - `noseLeftCorner` (1)
+     - `rightCheek` (1)
+     - `leftCheek` (1)
+
+## Implementation approaches
+
+### Key mesh points to use
+![Map of the 486 mesh points](mesh_map.jpg)
+- **Left eye edges:** 362 (inner), 263 (outer) / 386 (top), 374 (bottom)
+- **Right eye edges:** 133 (inner), 33 (outer) / 159 (top), 145 (bottom)
+- **Nose:** 1 (tip), 358 (left), 129 (right)
+
+Full list correlating these point indices and the coordinates for each feature in `annotations` is in [`annotations.json`](./annotations.json). (The `silhouette` will obviously change each frame, but the other numbered points should match.)
+
+### First approach
+
+Get point `1` of `scaledMesh` in `prediction`, which is roughly the tip of the nose. `x` is `180` when pointing straight ahead (on Firefox), so moving back and forth moves left and right. Process the `y` in a similar fashion to get the up and down.
+
+> **Result:** 
+> - It's calibrated on Firefox, where the coordinate system is different from Chrome and Safari because the _video is sized differently_. 
+> - The only thing that matters is the position of the tip of your nose, so moving your head position cheats it.
+> - If a second face prediction is added (tracking two faces), the scaled mesh appears to be on a coordinate system for the bounding box of both faces.
+
+### Second approach
+
+- Add an overlay to coach the user where to put their head, in the center of the camera view.
+
+- Calculate angles of the direction the face is pointing. _Yikes, it's linear algebra, all over again._
+
+    - Get the following points from `annotations`: 
+
+        - **M**: `midwayBetweenEyes` (point 168), 
+        - **R**: `noseRightCorner` (point 98),
+        - **L**: `noseLeftCorner` (point 327)
+
+    - Make a plane to find an orthogonal unit vector. 
+
+        1. make vector MR [Rx-Mx,Ry-My,Rz-Mz] and vector ML (L-M, same way)
+        2. use right-hand rule: cross product MR x ML = vector pointing (hopefully) straight out from the face 
+
+                  cross(A,B) = [ (Ay*Bz - Az*By), (Az*Bx - Ax*Bz), (Ax*By - Ay*Bx)]
+
+        3. reduce this to a unit vector by 
+
+    - Calculate the angles _alpha_ (around the x axis, causing y up and down movement) and _beta_ (around the y axis, causing left and right movement) 
+
+### Future approaches
+- get position of face in camera frame, direction of face relative to the camera, direction of iris relative to face, and make a vector to figure out where the person is looking.
+
+## Why not use Tracking.js / face-api.js / WebGazer.js?
+
+Initially, we were looking at [WebGazer.js](https://github.com/brownhci/WebGazer), a JavaScript library from researchers at Brown University designed to (for example) help create eyetracking heatmaps. 
+
+WebGazer actually uses Facemesh under the hood, but combines it with their own iris tracking model, and heavily biases it toward where the mouse cursor position is. In our experiments, it required a lot of calibration (for our intended purposes), and was not as reliable when a mouse was not being used.
+
+We also looked into [Tracking.js](https://trackingjs.com/), but it not been updated by its maintainer in two years. [face-api.js](https://github.com/justadudewhohacks/face-api.js/blob/master/README.md) was another viable option, but the performance was slightly slower than Facemesh.
+
+Furthermore, Google recently announced an [iris tracking model](https://google.github.io/mediapipe/solutions/iris) for Mediapipe that looks quite accurate. The tfjs team is still working to [make this easily available in the  JavaScript library](https://github.com/tensorflow/tfjs-models/pull/508).
---
+++ docs/gaze/src/index.js
@@ -0,0 +1,294 @@
+/**
+ * Adapted from Tensorflow.js' facemesh demo (Apache 2.0 licensed) at
+ * https://github.com/tensorflow/tfjs-models/tree/master/facemesh/demo
+ */
+
+import * as facemesh from '@tensorflow-models/facemesh';
+import * as tf from '@tensorflow/tfjs-core';
+import '@tensorflow/tfjs-backend-webgl';
+
+import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
+
+tfjsWasm.setWasmPath(
+  `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${tfjsWasm.version_wasm}/dist/tfjs-backend-wasm.wasm`);
+
+function isMobile() {
+  const isAndroid = /Android/i.test(navigator.userAgent);
+  const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  return isAndroid || isiOS;
+}
+
+function isFirefox() {
+  // Firefox has a different coordinate system than Chrome/Safari
+  const isFF = /Firefox/i.test(navigator.userAgent);
+  return isFF; 
+}
+
+function drawPath(ctx, points, closePath) {
+  const region = new Path2D();
+  region.moveTo(points[0][0], points[0][1]);
+  for (let i = 1; i < points.length; i++) {
+    const point = points[i];
+    region.lineTo(point[0], point[1]);
+  }
+
+  if (closePath) {
+    region.closePath();
+  }
+  ctx.stroke(region);
+}
+
+let model, ctx, videoWidth, videoHeight, video, canvas;
+let predix = {};
+
+const VIDEO_SIZE = 240;
+const mobile = isMobile();
+// Don't render the point cloud on mobile in order to maximize performance and
+// to avoid crowding limited screen space.
+const renderPointcloud = mobile === false;
+const state = {
+  backend: 'wasm', // or 'webgl' or 'cpu', set with `await tf.setBackend(backend)`
+  maxFaces: 1, // up to 20, set with `await facemesh.load({maxFaces: val})`
+  triangulateMesh: false
+};
+const outputDiv = document.querySelector('#console');
+const hud = document.querySelector('#hud');
+const hudCursor = document.querySelector('#cursor');
+
+if (renderPointcloud) {
+  state.renderPointcloud = true;
+}
+
+async function setupCamera() {
+  video = document.getElementById('video');
+
+  const stream = await navigator.mediaDevices.getUserMedia({
+    'audio': false,
+    'video': {
+      facingMode: 'user',
+      // Only setting the video to a specified size in order to accommodate a
+      // point cloud, so on mobile devices accept the default size.
+      width: mobile ? undefined : VIDEO_SIZE,
+      height: mobile ? undefined : VIDEO_SIZE
+    },
+  });
+  video.srcObject = stream;
+
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+
+// OLD FUNCTION moveCursor just mirrors your nose tip 
+// position in the window.
+const MAX_X = 215;
+const MIN_X = 145;
+const MAX_Y = 140;
+const MIN_Y = 130;
+
+function moveCursor(point) {
+
+  let x = point[0]; 
+  let y = point[1];
+  // console.log('Unfiltered x: ',x,', y: ',y);
+  // let z = point[2];
+  // constrain the points
+  if (x > MAX_X) x = MAX_X;
+  if (x < MIN_X) x = MIN_X;
+  if (y > MAX_Y) y = MAX_Y;
+  if (y < MIN_Y) y = MIN_Y;
+  const cursorX = (x - MIN_X) / (MAX_X - MIN_X);
+  const cursorY = (y - MIN_Y) / (MAX_Y - MIN_Y);
+  
+  hudCursor.style.right = `calc( ${cursorX*100}% - 10px)`;
+  hudCursor.style.top = `${cursorY*hud.clientHeight}px`;
+  // console.log(`x: ${x} or ${cursorX}, y: ${y} or ${cursorY}`);
+}
+
+function crossProduct(a,b) {
+  return [
+    a[1]*b[2] - a[2]*b[1],
+    a[2]*b[0] - a[0]*b[2],
+    a[0]*b[1] - a[1]*b[0]
+  ]
+}
+
+// New function: calculates angle of your face direction.
+const MAX_H = 25;
+const MIN_H = -25;
+const MAX_V = 15;
+const MIN_V = -15;
+
+function moveCursorVector(M,L,R) {
+  outputDiv.innerHTML = '';
+  // M, L, R are all 3D points
+  const ML = [L[0]-M[0], L[1]-M[1], L[2]-M[2]];
+  const MR = [R[0]-M[0], R[1]-M[1], R[2]-M[2]];
+  // Get the cross product of MR x ML.
+  const dir = crossProduct(MR,ML);
+  const len = Math.sqrt(dir[0]*dir[0] + dir[1]*dir[1] + dir[2]*dir[2]);
+  const dirUnit = [dir[0]/len, dir[1]/len, dir[2]/len];
+  // Allow correction of the unit vector since plane may 
+  // not be perfectly aligned with camera when normalized.
+  const unit = [dirUnit[0]+0, dirUnit[1]+0, dirUnit[2]+0];
+  const horizAngle = Math.atan2(unit[0],-1*unit[2]) * 180/Math.PI;
+  const vertAngle  = Math.atan2(unit[1],-1*unit[2]) * 180/Math.PI;
+  // Show the output of the angles (for debugging).
+  outputDiv.innerHTML += 'horiz: '+horizAngle+'\n';
+  outputDiv.innerHTML += 'vert: '+vertAngle+'\n';
+  // Move the cursor!
+  const totalWidth = hud.clientWidth;
+  const totalHeight = hud.clientHeight;
+  let cursorX = horizAngle + MAX_H;
+  if (cursorX < 0) cursorX = 0;
+  if (cursorX > MAX_H*2) cursorX = MAX_H*2;
+  cursorX = cursorX / (MAX_H - MIN_H);
+  let cursorY = vertAngle + MAX_V;
+  if (cursorY < 0) cursorY = 0;
+  if (cursorY > MAX_V*2) cursorY = MAX_V*2;
+  cursorY = cursorY / (MAX_V - MIN_V);
+  
+  hudCursor.style.right = `calc( ${cursorX*100}% - 10px)`;
+  hudCursor.style.top = `${cursorY*hud.clientHeight}px`;
+  
+
+}
+
+async function renderPrediction() {
+  const predictions = await model.estimateFaces(video);
+  ctx.drawImage(
+      video, 0, 0, videoWidth, videoHeight, 0, 0, canvas.width, canvas.height);
+
+  if (predictions.length > 0) {
+    predictions.forEach(prediction => {
+      const keypoints = prediction.scaledMesh;
+      // *** for logging and debugging only:
+      predix = prediction;
+      // ***
+
+      if (state.triangulateMesh) {
+        for (let i = 0; i < TRIANGULATION.length / 3; i++) {
+          const points = [
+            TRIANGULATION[i * 3], TRIANGULATION[i * 3 + 1],
+            TRIANGULATION[i * 3 + 2]
+          ].map(index => keypoints[index]);
+
+          drawPath(ctx, points, true);
+        }
+      } else {
+        for (let i = 0; i < keypoints.length; i++) {
+          const x = keypoints[i][0];
+          const y = keypoints[i][1];
+
+          ctx.beginPath();
+          ctx.arc(x, y, 1 /* radius */, 0, 2 * Math.PI);
+          ctx.fill();
+        }
+      }
+      // IMPLEMENT THE DUMBEST VERSION OF GAZE DETECTION
+      // each coordinate in keypoints is an array [x,y,z]
+      // keypoints[1] is roughly the tip of the nose
+      // for a person looking head-on: 
+      //  its x ranges from about 215-145 (±35), centered on 180
+      //  its y ranges from about 130-140 (±10)
+      // moveCursor(keypoints[1])
+      moveCursorVector(
+        prediction.annotations.midwayBetweenEyes[0],
+        prediction.annotations.noseLeftCorner[0],
+        prediction.annotations.noseRightCorner[0]
+      );
+
+    });
+
+    // if (renderPointcloud && state.renderPointcloud && scatterGL != null) {
+    //   const pointsData = predictions.map(prediction => {
+    //     let scaledMesh = prediction.scaledMesh;
+    //     return scaledMesh.map(point => ([-point[0], -point[1], -point[2]]));
+    //   });
+
+    //   let flattenedPointsData = [];
+    //   for (let i = 0; i < pointsData.length; i++) {
+    //     flattenedPointsData = flattenedPointsData.concat(pointsData[i]);
+    //   }
+    //   const dataset = new ScatterGL.Dataset(flattenedPointsData);
+
+    //   if (!scatterGLHasInitialized) {
+    //     scatterGL.render(dataset);
+    //   } else {
+    //     scatterGL.updateDataset(dataset);
+    //   }
+    //   scatterGLHasInitialized = true;
+    // }
+  }
+  requestAnimationFrame(renderPrediction);
+};
+
+// DEBUG MODE: log out the prediction to the console on click
+document.addEventListener('click', (e) => {
+  // console.log(JSON.stringify(predix,null,2));
+  console.log(predix.scaledMesh);
+  console.log(predix.annotations);
+  const numbers = {};
+  for (let group in predix.annotations) {
+    numbers[group] = [];
+    for (let i = 0; i < predix.annotations[group].length; i++) {
+      const point = predix.annotations[group][i];
+      numbers[group].push(predix.scaledMesh.findIndex(item => JSON.stringify(item) === JSON.stringify(point)));
+      console.log(`${group}[${i}] is ${point} which is equal to ${numbers[group][i]}: ${predix.scaledMesh[numbers[group][i]]}`)
+    }
+  }
+  console.log(numbers);
+  console.log(JSON.stringify(numbers));
+});
+
+async function main() {
+  await tf.setBackend(state.backend);
+  // setupDatGui();
+
+  const backendSelect = document.querySelector('select#backend');
+  backendSelect.addEventListener('change', async (event) => {
+    const newSelection = backendSelect.selectedOptions[0].value;
+    console.log(`changing backend to ${newSelection}`);
+    await tf.setBackend(newSelection)
+    state.backend = newSelection;
+  });
+
+  //stats.showPanel(0);  // 0: fps, 1: ms, 2: mb, 3+: custom
+  //document.getElementById('main').appendChild(stats.dom);
+
+  await setupCamera();
+  video.play();
+  videoWidth = video.videoWidth;
+  videoHeight = video.videoHeight;
+  video.width = videoWidth;
+  video.height = videoHeight;
+
+  canvas = document.getElementById('output');
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+  const canvasContainer = document.querySelector('.canvas-wrapper');
+  canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
+
+  ctx = canvas.getContext('2d');
+  ctx.translate(canvas.width, 0);
+  ctx.scale(-1, 1);
+  ctx.fillStyle = '#32EEDB';
+  ctx.strokeStyle = '#32EEDB';
+  ctx.lineWidth = 0.5;
+
+  model = await facemesh.load({maxFaces: state.maxFaces});
+  renderPrediction();
+
+  // if (renderPointcloud) {
+  //   document.querySelector('#scatter-gl-container').style =
+  //       `width: ${VIDEO_SIZE}px; height: ${VIDEO_SIZE}px;`;
+
+  //   scatterGL = new ScatterGL(
+  //       document.querySelector('#scatter-gl-container'),
+  //       {'rotateOnStart': false, 'selectEnabled': false});
+  // }
+};
+
+main();
